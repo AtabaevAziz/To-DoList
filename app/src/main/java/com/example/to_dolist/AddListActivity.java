a9cf86ba46d3dbeb7db1e54e07f200c2599c8e10
aziz <19atabaev.aziz@gmail.com>
2020-10-24 18:45:37 +0500

    To-do list: Begin

--- app/src/main/java/com/example/to_dolist/AddListActivity.java
+++ app/src/main/java/com/example/to_dolist/AddListActivity.java
@@ -1,4 +1,223 @@
 package com.example.to_dolist;
 
-public class AddListActivity extends MainActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.to_dolist.R;
+import com.example.to_dolist.data.ToDoListContract;
+
+public class AddListActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor>{
+
+
+    private static final int EDIT_MEMBER_LOADER = 111;
+    Uri currentTaskUri;
+
+    private EditText describeTheTaskEditText;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_list);
+
+        Intent intent = getIntent();
+
+        currentTaskUri = intent.getData();
+
+        if (currentTaskUri == null) {
+            setTitle("Add a Task");
+            invalidateOptionsMenu();
+        } else {
+            setTitle("Edit the Task");
+            getSupportLoaderManager().initLoader(EDIT_MEMBER_LOADER,
+                    null, this);
+        }
+
+        describeTheListEditText = findViewById(R.id.dataListView);
+
+    }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        super.onPrepareOptionsMenu(menu);
+
+        if (currentTaskUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.delete_list);
+            menuItem.setVisible(false);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.edit_task_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_task:
+                saveTask();
+                return  true;
+            case R.id.delete_task:
+                showDeleteTaskDialog();
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void saveTask() {
+
+        String editText = describeTheTaskEditText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(editText)) {
+            Toast.makeText(this,
+                    "Input the describe the text",
+                    Toast.LENGTH_LONG).show();
+            return;
+
+        }
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ToDoListContract.TaskEntry.COLUMN_DESCRIBE_THE_TASK, editText);
+
+        if (currentTaskUri == null) {
+            ContentResolver contentResolver = getContentResolver();
+            Uri uri = contentResolver.insert(ToDoListContract.TaskEntry.CONTENT_URI,
+                    contentValues);
+
+            if (uri == null) {
+                Toast.makeText(this,
+                        "Insertion of data in the table failed",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this,
+                        "Data saved", Toast.LENGTH_LONG).show();
+
+            }
+        } else {
+            int rowsChanged = getContentResolver().update(currentTaskUri,
+                    contentValues, null, null);
+
+            if (rowsChanged == 0) {
+                Toast.makeText(this,
+                        "Saving of data in the table failed",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this,
+                        "Task updated", Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+
+        String[] projection = {
+                ToDoListContract.TaskEntry._ID,
+                ToDoListContract.TaskEntry.COLUMN_DESCRIBE_THE_TASK,
+
+        };
+
+        return new CursorLoader(this,
+                currentTaskUri,
+                projection,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        if (cursor.moveToFirst()) {
+            int firstNameColumIndex = cursor.getColumnIndex(
+                    ToDoListContract.TaskEntry.COLUMN_DESCRIBE_THE_TASK
+            );
+
+
+            String describeTheTask = cursor.getString(firstNameColumIndex);
+
+            describeTheTaskEditText.setText(describeTheTask);
+
+
+
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+
+    private void showDeleteTaskDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Do you want delete the member?");
+        builder.setPositiveButton("Delete",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteTask();
+                    }
+                });
+        builder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (dialog != null) {
+                            dialog.dismiss();
+                        }
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+    private void deleteTask() {
+        if (currentTaskUri != null) {
+            int rowsDeleted = getContentResolver().delete(currentTaskUri,
+                    null, null);
+
+            if (rowsDeleted == 0) {
+                Toast.makeText(this,
+                        "Deleting of task from the table failed",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this,
+                        "Task is deleted",
+                        Toast.LENGTH_LONG).show();
+            }
+
+            finish();
+        }
+    }
+
 }
--- app/src/main/java/com/example/to_dolist/MainActivity.java
+++ app/src/main/java/com/example/to_dolist/MainActivity.java
@@ -1,14 +1,104 @@
 package com.example.to_dolist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
+
+import com.example.to_dolist.data.ToDoListContract;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int TASK_LOADER = 123;
+    ListCursorAdapter listCursorAdapter;
+
+    ListView dataListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataListView = findViewById(R.id.dataListView);
+
+        FloatingActionButton floatingActionButton =
+                findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        AddListActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        listCursorAdapter = new ListCursorAdapter(this,
+                null, false);
+        dataListView.setAdapter(listCursorAdapter);
+
+        dataListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Intent intent = new Intent(MainActivity.this,
+                        AddListActivity.class);
+                Uri currentMemberUri = ContentUris
+                        .withAppendedId(ToDoListContract.TaskEntry.CONTENT_URI, id);
+                intent.setData(currentMemberUri);
+                startActivity(intent);
+
+            }
+        });
+
+        getSupportLoaderManager().initLoader(TASK_LOADER,
+                null, this);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        String[] projection = {
+                ToDoListContract.TaskEntry._ID,
+                ToDoListContract.TaskEntry.COLUMN_DESCRIBE_THE_TASK,
+
+        };
+
+        CursorLoader cursorLoader = new CursorLoader(this,
+                ToDoListContract.TaskEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null
+        );
+        return cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+
+        listCursorAdapter.swapCursor(cursor);
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+        listCursorAdapter.swapCursor(null);
+
     }
 }
