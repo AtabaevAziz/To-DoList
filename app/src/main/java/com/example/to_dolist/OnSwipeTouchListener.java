e5ec36cd39fb9db7d115d0707257c2ef6ce43f6e
aziz <19atabaev.aziz@gmail.com>
2020-11-23 11:56:40 +0500

    To-do list: delete by swipe(without animation)

--- app/src/main/java/com/example/to_dolist/OnSwipeTouchListener.java
+++ app/src/main/java/com/example/to_dolist/OnSwipeTouchListener.java
@@ -9,6 +9,7 @@ import android.view.View.OnTouchListener;
 
 public class OnSwipeTouchListener implements OnTouchListener {
 
+
     public final GestureDetector gestureDetector;
 
     public OnSwipeTouchListener (Context ctx){
